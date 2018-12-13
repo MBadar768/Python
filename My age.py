1b6821c9a5778d1d2902a5022436b3040f6ef6af
MBadar768 <44946634+MBadar768@users.noreply.github.com>
2018-12-13 23:25:11 +0500

    Create My age.py

---
+++ My age.py
@@ -0,0 +1,11 @@
+import math
+
+print('What is your age:')
+myAge = input()
+print('Your age is :', myAge)
+
+a = int(input('Enter your number:'))
+b = math.sqrt(a)
+c = math.sin(a)
+print('Square root is :', b)
+print('Sin function answer is:', c)
